8b19340739bed1f12b7622f486970d030f0b1ddc
Dagner <monolith.gaia@gmail.com>
2021-02-05 09:30:25 -0600

    Se agrego estructura de directorios y código base

---
+++ src/play_scene.py
@@ -0,0 +1,26 @@
+from scene import Scene
+import pygame
+
+class PlayScene(Scene):
+    def __init__(self, app):
+        self.app = app
+        self.screen = app.screen
+        super().__init__('PlayScene')
+
+    def start(self):
+        print('Se inicia:', self.name)
+    
+    def process_events(self, event):
+        if event.type == pygame.KEYDOWN:
+            self.app.change_scene('intro')
+            print('Se presiono una tecla')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill((255, 255, 255))
+        pygame.draw.circle(self.screen,(0, 0, 255), (self.app.width/2, self.app.height/2), 30)
+        
+    def exit(self):
+        print('Termina:', self.name)
