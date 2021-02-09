88ae93b8aaaf64baf15292ad0955fa1a1d9c1873
Dagner <monolith.gaia@gmail.com>
2021-02-09 09:48:59 -0600

    Se agrega clase ship y se dibuja nave en play_scene

--- src/play_scene.py
+++ src/play_scene.py
@@ -1,10 +1,12 @@
 from scene import Scene
+from ship import Ship
 import pygame
 
 class PlayScene(Scene):
     def __init__(self, app):
         self.app = app
         self.screen = app.screen
+        self.ship = Ship(app)
         super().__init__('PlayScene')
 
     def start(self):
@@ -19,8 +21,9 @@ class PlayScene(Scene):
         pass
 
     def draw(self):
-        self.screen.fill((255, 0, 255))
-        pygame.draw.circle(self.screen,(0, 0, 255), (self.app.width/2, self.app.height/2), 30)
-        
+        self.screen.fill((255, 255, 255))
+        #pygame.draw.circle(self.screen,(0, 0, 255), (self.app.width/2, self.app.height/2), 30)
+        self.ship.draw()
+
     def exit(self):
         print('Termina:', self.name)
